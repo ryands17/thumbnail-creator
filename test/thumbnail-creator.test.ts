f94157dd29b15c0d627bd459295af0bc95c0e309
Ryan <dsouza.ryan.11@gmail.com>
2021-02-11 17:41:34 +0530

    Initial commit

---
+++ test/thumbnail-creator.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as ThumbnailCreator from '../lib/thumbnail-creator-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new ThumbnailCreator.ThumbnailCreatorStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
