a736305623feb73f92fd5fe40244dbd41e496097
Ryan <dsouza.ryan.11@gmail.com>
2021-02-11 19:03:55 +0530

    add dockerfile and context

--- bin/thumbnail-creator.ts
+++ bin/thumbnail-creator.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ThumbnailCreatorStack } from '../lib/thumbnail-creator-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { ThumbnailCreatorStack } from '../lib/thumbnail-creator-stack'
 
-const app = new cdk.App();
-new ThumbnailCreatorStack(app, 'ThumbnailCreatorStack',{env: {region: app.node.tryGetContext('region')}});
+const app = new cdk.App()
+new ThumbnailCreatorStack(app, 'ThumbnailCreatorStack', {
+  env: { region: app.node.tryGetContext('region') },
+})
